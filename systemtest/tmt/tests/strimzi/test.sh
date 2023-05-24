28f57d7e3d93652608b736721a6099229212037c
David Kornel <kornys@outlook.com>
2023-05-24 10:58:50 +0200

    fixup! Change manifests when we build arch architecture
    Signed-off-by: David Kornel <kornys@outlook.com>

--- systemtest/tmt/tests/strimzi/test.sh
+++ systemtest/tmt/tests/strimzi/test.sh
@@ -5,9 +5,6 @@ cd ../../../../
 
 eval $(minikube docker-env)
 
-ARCH=$(uname -m)
-if [[ ${ARCH} == "aarch64" ]] || [[ ${ARCH} == "arm64" ]]; then export DOCKER_TAG="${DOCKER_TAG}-arm64"; fi
-
 #run tests
 export DOCKER_REGISTRY="$(kubectl get service registry -n kube-system -o=jsonpath='{.spec.clusterIP}'):80"
 mvn verify -pl systemtest -P ${TEST_PROFILE} \
